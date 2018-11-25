feb5d520441acbb3fcbef8aa285c3ae003445706
Seth Etter <sethetter@gmail.com>
2018-11-25 13:27:37 -0600

    urlshort: cmd accepts yaml file

--- urlshort/README.md
+++ urlshort/README.md
@@ -4,7 +4,7 @@ A small web service for handling short urls.
 
 # Next
 
-- Accept a yaml file arg and use that mapping instead of the default mapping
+- Set up a basic logging middleware
 - Create a JSONHandler func and optionally accept a json file arg (allow only one)
 - Create a handler that reads from boltdb, behind a flag (db name? file location? idk)
 - Create an admin page behind basic auth to crud url mappings in the boltdb
--- urlshort/cmd/urlshort/main.go
+++ urlshort/cmd/urlshort/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -9,21 +10,38 @@ import (
 )
 
 var (
-	port = flag.String("port", "3927", "port to serve on")
+	port     = flag.String("port", "3927", "port to serve on")
+	yamlFile = flag.String("yaml", "", "path to yaml file with url mappings")
 )
 
 func main() {
-	urlmap := map[string]string{
-		"ow": "https://openwichita.org",
-		"se": "https://seth.computer",
-	}
+	flag.Parse()
+
 	fallback := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 	})
-	urlshortHandler, err := urlshort.MapHandler(urlmap, fallback)
-	if err != nil {
-		log.Fatalf("Failed to create handler: %v", err)
+
+	var err error
+	var urlshortHandler http.Handler
+	if *yamlFile != "" {
+		var yml []byte
+		yml, err = ioutil.ReadFile(*yamlFile)
+		if err != nil {
+			log.Fatalf("Error reading YAML file: %v", err)
+		}
+		urlshortHandler, err = urlshort.YAMLHandler(yml, fallback)
+	} else {
+		urlmap := map[string]string{
+			"ow": "https://openwichita.org",
+			"se": "https://seth.computer",
+		}
+		urlshortHandler, err = urlshort.MapHandler(urlmap, fallback)
 	}
+	if err != nil {
+		log.Fatalf("Failed to create default handler: %v", err)
+	}
+
 	http.Handle("/", urlshortHandler)
+	log.Printf("Serving on port %s\n", *port)
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
